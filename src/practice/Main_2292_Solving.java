fddbd9ce0c0c949dfe084f20db4168a67ab1b598
KimBeomJoo <rebirth0116@gmail.com>
2020-06-26 12:47:35 +0900

    2020.06.26

--- src/practice/Main_2292_Solving.java
+++ src/practice/Main_2292_Solving.java
@@ -4,16 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_2292 {
+public class Main_2292_Solving {
 	static int n, cnt, cnt2;
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i<=n; i++) {
-			if(i>n && )
-		}
 		
 	}
 
