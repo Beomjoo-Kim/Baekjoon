d1d7827240d45903480fb99138b5c1bf8ef38ef3
KimBeomJoo <rebirth0116@gmail.com>
2020-07-08 18:56:41 +0900

    2020.07.08

--- src/practice/Main_4949_check.java
+++ src/practice/Main_4949_check.java
@@ -3,7 +3,7 @@ package practice;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main_4949_solving {
+public class Main_4949_check {
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -21,7 +21,7 @@ public class Main_4949_solving {
 				if(temp == '(' || temp == '[') {
 					st.push(temp);
 				}else if(temp ==')' || temp ==']') {
-					if(st.peek()!='(' && temp == ']' || st.peek() !='[' && temp == ')' || st.isEmpty()) {
+					if(st.isEmpty() || st.peek()!='(' && temp == ')' || st.peek() !='[' && temp == ']') {
 						st.push(temp);
 						break;
 					}
