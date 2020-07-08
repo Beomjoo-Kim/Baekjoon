d68b78dd1a309055027a7640b7450613e945bc5b
KimBeomJoo <rebirth0116@gmail.com>
2020-07-08 17:57:16 +0900

    2020.07.08

---
+++ src/practice/Main_4949_solving.java
@@ -0,0 +1,38 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main_4949_solving {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Stack<Character> st = new Stack<Character>();
+		char temp;
+		while(true) {
+			st.clear();
+			String input = sc.nextLine();
+			if(input.equals(".")) {
+				break;
+			}
+			for(int i = 0; i<input.length(); i++) {
+				temp = input.charAt(i);
+				
+				if(temp == '(' || temp == '[') {
+					st.push(temp);
+				}else if(temp ==')' || temp ==']') {
+					if(st.peek()!='(' && temp == ']' || st.peek() !='[' && temp == ')' || st.isEmpty()) {
+						st.push(temp);
+						break;
+					}
+					st.pop();
+				}
+			}
+			
+			if(st.isEmpty()) {
+				System.out.println("yes");
+			}else System.out.println("no");
+		}
+		
+	}
+}
